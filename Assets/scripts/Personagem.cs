5613e7fd959b32260693da56ce3795a2072679be
Denylson Melo <143313+denylsonmelo@users.noreply.github.com>
2025-04-29 18:44:13 -0300

    Add initial project settings and configuration files
    - Created ProjectVersion.txt to specify the editor version.
    - Added QualitySettings.asset to define various quality levels for the project.
    - Introduced SceneTemplateSettings.json for managing scene templates and dependencies.
    - Created ShaderGraphSettings.asset for shader graph configurations.
    - Added TagManager.asset to manage tags and layers within the project.
    - Introduced TimeManager.asset to configure time settings for the project.
    - Created URPProjectSettings.asset for Universal Render Pipeline settings.
    - Added UnityConnectSettings.asset to manage Unity services and analytics settings.
    - Introduced VFXManager.asset for visual effects settings.
    - Created VersionControlSettings.asset to manage version control preferences.
    - Added XRSettings.asset for managing XR device settings.

---
+++ Assets/scripts/Personagem.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+//[AnimatorController]
+public class Personagem : MonoBehaviour
+{
+
+    [HideInInspector]
+    public int variavel = 1;
+    AnimatorController animacao;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("dentr do start");
+        animacao = GetComponent<AnimatorController>();  SetBool("mover", true)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(variavel<10)
+        {
+           Debug.Log("dentro d update: ");
+           variavel++; 
+        }
+    }
+
+    private void Awake() {
+        Debug.Log("dentr do awake");
+        
+    }
+
+    private void FixedUpdate() {
+        
+    }
+}
