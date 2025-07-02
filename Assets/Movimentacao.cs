cfa615be3f90bf004a0df872d66f69477ef62adf
Denylson Melo <143313+denylsonmelo@users.noreply.github.com>
2025-07-02 20:44:25 -0300

    add external assets

---
+++ Assets/Movimentacao.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Movimentacao : MonoBehaviour
+{
+
+    public Transform target;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GetComponent<UnityEngine.AI.NavMeshAgent>().SetDestination(target.position);
+        //Debug
+
+    }
+}
